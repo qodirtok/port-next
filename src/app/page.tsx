62565f7d3803798d789e23ebbb2cfb50f2f9a06e
qodirtok <qodirtok@gmail.com>
2023-05-25 10:17:32 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,8 +53,8 @@ export default function Home() {
             </h3>
             <p className="text-md py-5 leading-8 md:text-xl max-w-lg mx-auto">
               I&apos;m a developer with 3 year experience, speciality in
-              developing E-commerce apps for several companies. currently
-              working as a backend Engineer on from{" "}
+              developing eCommerce apps for several companies. currently working
+              as a backend Engineer on from{" "}
               <span className="text-blue-400"> Malang East Java</span>.
             </p>
           </div>
@@ -87,7 +87,8 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py1">Services</h3>
             <p className="text-md py2 leading-8">
-              since i begining of my journey <span>agencies</span>
+              since i begining of my journey{" "}
+              <span className="text-blue-400">agencies</span>
             </p>
           </div>
           <div className="lg:flex gap-10">
@@ -95,21 +96,21 @@ export default function Home() {
               job="Student Intern"
               company="University Of Muhammadiyah Malang"
               date="May 2015 - Aug 2015"
-              about="asd"
+              about={["asd"]}
               system={["Linux", "Debian", "Apache"]}
             />
             <ListService
               job="Student Intern"
               company="Dinas Komunikasi dan Informatika"
               date="Jan 2019 - Feb 2019"
-              about="asd"
+              about={["asd"]}
               system={["PHP", "CodeIgniter", "MySQL", "JavaScript", "HTML/CSS"]}
             />
             <ListService
               job="Fulltime Software Engineer"
               company="Icube by Sirclo"
               date="Jun 2020 - now"
-              about="asd"
+              about={["asd"]}
               system={[
                 "PHP",
                 "Magento 2",
@@ -130,31 +131,31 @@ export default function Home() {
             <ListPorto
               name="Sistem Perpustakaan"
               date="2023"
-              descriptions="about"
+              descriptions={["asd"]}
               system={["php", "MySQL", "Laravel", "Bootstrap"]}
             />
             <ListPorto
               name="POS (AGROMART)"
               date="2023"
-              descriptions="about"
+              descriptions={["asd"]}
               system={["php", "MySQL", "CodeIgniter", "Bootstrap"]}
             />
             <ListPorto
               name="POS (MIDOS)"
               date="2023"
-              descriptions="about"
+              descriptions={["asd"]}
               system={["php", "MySQL", "CodeIgniter", "Bootstrap"]}
             />
             <ListPorto
               name="SPBE (Sistem Pemerintahan Berbasis Elektronik)"
               date="2023"
-              descriptions="about"
+              descriptions={["asd"]}
               system={["php", "MySQL", "CodeIgniter", "Bootstrap"]}
             />
             <ListPorto
               name="SIKOPMA (Sistem Informasi Koperasi Mahasiswa)"
               date="2023"
-              descriptions="about"
+              descriptions={["asd"]}
               system={["php", "MySQL", "CodeIgniter", "Bootstrap"]}
             />
           </div>
@@ -170,7 +171,10 @@ const ListService = (params) => {
       <h3 className="text-lg font-medium pt-8 pb-0">{params.job}</h3>
       <h4 className="py-1 text-lg font-thin pt-1 pb-0">{params.company}</h4>
       <span className="py-1 text-xs font-serif pt-1 pb-2">{params.date}</span>
-      <p className="py-1">{params.about}</p>
+      <div>
+        <ul>{params.about.map((about) => rollCallAbout(about))}</ul>
+      </div>
+      {/* <p className="py-1 text-justify">{params.about}</p> */}
       <div className="flex flex-col gap-1 py-2 lg:flex-row lg:flex-wrap">
         {params.system.map((name) => rollCall(name))}
       </div>
@@ -183,7 +187,15 @@ const ListPorto = (params) => {
     <div className="text-center shadow-lg p-10 rounded-xl my-5 ">
       <h3 className="text-lg font-medium pt-8 pb-0">{params.name}</h3>
       <span className="py-1 text-xs font-serif pt-1 pb-2">{params.date}</span>
-      <p className="py-1">{params.descriptions}</p>
+      <div>
+        <ul>
+          {params.descriptions.map((descriptions) =>
+            rollCallAbout(descriptions)
+          )}
+        </ul>
+      </div>
+      {/* <p className="py-1 text-justify">{params.descriptions}</p> */}
+      {/* <p className="py-1 text-justify">{params.descriptions}</p> */}
       <div className="flex flex-col gap-1 py-2 lg:flex-row lg:flex-wrap">
         {params.system.map((name) => rollCall(name))}
       </div>
@@ -191,8 +203,18 @@ const ListPorto = (params) => {
   );
 };
 
+const rollCallAbout = (elements) => {
+  return (
+    <li>
+      <p className="py-0 text-justify">{elements}</p>
+    </li>
+  );
+};
+
 const rollCall = (elements) => {
   return (
-    <span className="bg-cyan-400 rounded-md py-1 px-1 text-xs">{elements}</span>
+    <span className="bg-cyan-400 rounded-md py-1 px-1 text-xs text-justify">
+      {elements}
+    </span>
   );
 };
