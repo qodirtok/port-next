3a839e90553b91140990f2f6d32d09746a30533a
qodirtok <qodirtok@gmail.com>
2023-05-27 11:36:34 +0700

    part 14

--- src/app/assets/component/section/expertise.tsx
+++ src/app/assets/component/section/expertise.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 import { log } from "console";
 import Magento from "../icon/expertice/magento.jpg";
 import Php from "../icon/expertice/php.jpg";
+import JScript from "../icon/expertice/jscript.jpg";
+import HTML from "../icon/expertice/html.jpg";
+import Css from "../icon/expertice/css.jpg";
+import Mysql from "../icon/expertice/mysql.png";
+import Gql from "../icon/expertice/gql.png";
+import RestApi from "../icon/expertice/restapi.png";
+import XMl from "../icon/expertice/xml.png";
+import Docker from "../icon/expertice/docker.jpg";
 
 export default function Expertise() {
   return (
@@ -47,20 +55,19 @@ const TextExpertise = () => {
 
 const ItemsExpertise = () => {
   return (
-    <div className="flex flex-wrap items-end p-0 gap-[40px]">
+    <div className="flex flex-wrap items-end justify-between p-0 gap-[40px]">
       <Items
         items={[
           { logo: <Image src={Magento} alt="" />, name: "Magento" },
           { logo: <Image src={Php} alt="" />, name: "PHP" },
-          { logo: <Image src={""} alt="" />, name: "HTML" },
-          { logo: <Image src={""} alt="" />, name: "CSS" },
-          { logo: <Image src={""} alt="" />, name: "Java Script" },
-          { logo: <Image src={""} alt="" />, name: "MySQL" },
-          { logo: <Image src={""} alt="" />, name: "GraphQL" },
-          { logo: <Image src={""} alt="" />, name: "Rest API" },
-          { logo: <Image src={""} alt="" />, name: "XML" },
-          { logo: <Image src={""} alt="" />, name: "Docker" },
-          { logo: <Image src={""} alt="" />, name: "Linux" },
+          { logo: <Image src={HTML} alt="" />, name: "HTML" },
+          { logo: <Image src={Css} alt="" />, name: "CSS" },
+          { logo: <Image src={JScript} alt="" />, name: "Java Script" },
+          { logo: <Image src={Mysql} alt="" />, name: "MySQL" },
+          { logo: <Image src={Gql} alt="" />, name: "GraphQL" },
+          { logo: <Image src={RestApi} alt="" />, name: "Rest API" },
+          { logo: <Image src={XMl} alt="" />, name: "XML" },
+          { logo: <Image src={Docker} alt="" />, name: "Docker" },
         ]}
       />
     </div>
